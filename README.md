2969e1edf8d9e38cc8bdf292ac37fcb28d06c7df
nacen.dev <nacen.dev@gmail.com>
2022-03-19 21:24:31 +0800

    Complete Assignment 3
    Wrote a function that loops through the array and displays each book on the page.

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 - [x] 1. If you haven’t already, set up your project with skeleton HTML/CSS and JS files.
 
-2. All of your book objects are going to be stored in a simple array, so add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array. Your code should look something like this:
+- [x] 2. All of your book objects are going to be stored in a simple array, so add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array. Your code should look something like this:
 
 ```
 let myLibrary = [];
@@ -19,7 +19,7 @@ function addBookToLibrary() {
 }
 ```
 
-3. Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
+- [x] 3. Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”. It might help for now to manually add a few books to your array so you can see the display.
 
 4. Add a “NEW BOOK” button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read and anything else you might want.
 
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const myLibrary = [];
 
-function Book(name, id, pages, read, author) {
+function Book(name, pages, read, author) {
   this.name = name;
-  this.id = id;
   this.pages = pages;
   this.read = read;
   this.author = author;
@@ -11,3 +10,51 @@ function Book(name, id, pages, read, author) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
+
+let atomicHabits = new Book("Atomic Habits", 306, true, "James Clear");
+let atomicHabits2 = new Book("Atomic Habits", 306, true, "James Clear");
+let atomicHabits3 = new Book("Atomic Habits", 306, true, "James Clear");
+
+addBookToLibrary(atomicHabits);
+addBookToLibrary(atomicHabits2);
+addBookToLibrary(atomicHabits3);
+
+const books = document.querySelector(".books-display");
+console.log(books);
+
+function createBookElement(book) {
+  const { name, pages, read, author } = book;
+
+  const bookElement = document.createElement("div");
+  bookElement.className = "book card";
+
+  const bookTitle = document.createElement("h3");
+  bookTitle.textContent = name;
+  bookTitle.className = "book-title";
+  bookElement.appendChild(bookTitle);
+
+  const bookPages = document.createElement("p");
+  bookPages.textContent = `Pages: ${pages}`;
+  bookPages.className = "book-pages";
+  bookElement.appendChild(bookPages);
+
+  const readElement = document.createElement("p");
+  readElement.textContent = `Read: ${read ? "Have read" : "Not read yet"}`;
+  readElement.className = "book-read";
+  bookElement.appendChild(readElement);
+
+  const bookAuthor = document.createElement("p");
+  bookAuthor.textContent = `Author: ${author}`;
+  bookAuthor.className = "book-author";
+  bookElement.appendChild(bookAuthor);
+
+  return bookElement;
+}
+
+function addLibraryToBooksDisplay() {
+  myLibrary.forEach((book) => {
+    books.appendChild(createBookElement(book));
+  });
+}
+
+addLibraryToBooksDisplay();
